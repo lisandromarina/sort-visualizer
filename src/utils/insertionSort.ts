05b80ba04cdb95c44ee2dfd2666a45d9878037ed
lisandromarina <lisandromarina1@gmail.com>
2023-08-02 11:07:44 -0400

    add typescript types in insertion sort

--- src/utils/insertionSort.ts
+++ src/utils/insertionSort.ts
@@ -1,27 +1,27 @@
-export function getInsertionSortAnimations(array:any) {
-    let animations:any = [];
-    let helperArray = array.slice();
-    insertionSort(helperArray, animations);
-    return animations;
-  }
-  
-  function insertionSort(helperArray:any, animations:any) {
-    const length = helperArray.length;
-    for (let i = 1; i < length; i++) {
-      let position = helperArray[i];
-      let j = i - 1;
-      animations.push(["comparison1", j, i]);
-      animations.push(["comparison2", j, i]);
-      while (j >= 0 && helperArray[j] > position) {
-        animations.push(["overwrite", j + 1, helperArray[j]]);
-        helperArray[j + 1] = helperArray[j];
-        j = j - 1;
-        if (j >= 0) {
-          animations.push(["comparison1", j, i]);
-          animations.push(["comparison2", j, i]);
-        }
+export function getInsertionSortAnimations(array: number[]): any[] {
+  const animations: any[] = [];
+  let helperArray = array.slice();
+  insertionSort(helperArray, animations);
+  return animations;
+}
+
+function insertionSort(helperArray: number[], animations: any[]) {
+  const length = helperArray.length;
+  for (let i = 1; i < length; i++) {
+    let position = helperArray[i];
+    let j = i - 1;
+    animations.push(["comparison1", j, i]);
+    animations.push(["comparison2", j, i]);
+    while (j >= 0 && helperArray[j] > position) {
+      animations.push(["overwrite", j + 1, helperArray[j]]);
+      helperArray[j + 1] = helperArray[j];
+      j = j - 1;
+      if (j >= 0) {
+        animations.push(["comparison1", j, i]);
+        animations.push(["comparison2", j, i]);
       }
-      animations.push(["overwrite", j + 1, position]);
-      helperArray[j + 1] = position;
     }
+    animations.push(["overwrite", j + 1, position]);
+    helperArray[j + 1] = position;
   }
+}
